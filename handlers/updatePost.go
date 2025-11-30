2ddd451aa2a180b21e62fc66523e068a6c317e91
dima <dimazigulin2213@gmail.com>
2025-11-30 09:55:39 +0300

    добавил коментарии к коду

--- handlers/updatePost.go
+++ handlers/updatePost.go
@@ -6,21 +6,29 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// функция обнавления тела поста
 func UpdatePost(c *fiber.Ctx) error {
 	var post database.Post
 
+	// проверяем правильность JSON, полученного из запроса
+	// если он не верен, то возвращаем ошибку 400
 	if err := c.BodyParser(&post); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"error": "invalid JSON\n",
 		})
 	}
 
+	// проверяем правильность данных, записанных в структуру
+	// если они не верны, то возвращаем ошибку 400
 	if err := Validate.Struct(&post); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"error": "invalid JSON\n",
 		})
 	}
 
+	// проверяем существует ли такой user и есть ли у него пост
+	// если такого пользователя несуществует, то возвращаем ошибку 400
+	// в обратном случае обновляем данные
 	if body := database.DataBase[post.UserName]; body == "" {
 		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
 			"error": "user not found\n",
@@ -29,5 +37,6 @@ func UpdatePost(c *fiber.Ctx) error {
 		database.DataBase[post.UserName] = post.Body
 	}
 
+	// возвращаем положительный ответ
 	return c.SendStatus(fiber.StatusOK)
 }
